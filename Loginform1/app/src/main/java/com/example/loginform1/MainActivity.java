865b9e582f650300b86e797d38fa5a494fa988d3
b-5d3 <venkatesh33369@gmail.com>
2021-03-31 14:31:29 +0530

    yugu

---
+++ Loginform1/app/src/main/java/com/example/loginform1/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.loginform1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        TextView register = (TextView)findViewById(R.id.lnkRegister);
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, RegistrationActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
